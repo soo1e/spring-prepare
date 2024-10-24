a2daa63412a0076d4f4c6438562060cc34c3ab22
soo1e <gameswuon@gmail.com>
2024-10-24 16:11:15 +0900

    10/24

--- src/main/java/com/sparta/springprepare/controller/HelloController.java
+++ src/main/java/com/sparta/springprepare/controller/HelloController.java
@@ -1,12 +1,37 @@
 package com.sparta.springprepare.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class HelloController {
     @GetMapping("/api/hello")
+    @ResponseBody
     public String hello() {
-        return "Hello World!";
+        return "Hello World!!";
+    }
+
+    @GetMapping("/api/get")
+    @ResponseBody
+    public String get() {
+        return "Get Method 요청";
+    }
+
+    @PostMapping("/api/post")
+    @ResponseBody
+    public String post() {
+        return "Post Method 요청";
+    }
+
+    @PutMapping("/api/put")
+    @ResponseBody
+    public String put() {
+        return "Put Method 요청";
+    }
+
+    @DeleteMapping("/api/delete")
+    @ResponseBody
+    public String delete() {
+        return "Delete Method 요청";
     }
 }
